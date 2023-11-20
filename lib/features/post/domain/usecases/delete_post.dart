d4c1049339a1362ee5e33342a63be56b3d6d52de
BhMedRayen <rayenmohamed2310@gmail.com>
2023-11-20 23:52:04 +0100

    DeletePostUseCase

---
+++ lib/features/post/domain/usecases/delete_post.dart
@@ -0,0 +1,12 @@
+import 'package:dartz/dartz.dart';
+import '../repositories/post_repository.dart';
+import '../../../../core/failures/failures.dart';
+
+class DeletePostUseCase {
+  final PostsRepository repository;
+  DeletePostUseCase(this.repository);
+
+  Future<Either<Failure, Unit>> call(int PostId) async {
+    return await repository.deletPost(PostId);
+  }
+}

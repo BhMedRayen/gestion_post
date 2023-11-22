0c7edc0f1dac88436bcca182e84bd9aa3ca14baa
BhMedRayen <rayenmohamed2310@gmail.com>
2023-11-22 09:26:11 +0100

    UKNOWN message

--- lib/core/strings/messages.dart
+++ lib/core/strings/messages.dart
@@ -1,3 +1,4 @@
 const ADD_SUCCESS_MESSAGE = 'Post ajouté avec succées';
 const DELETE_SUCCESS_MESSAGE = 'Post supprimé avec succés';
 const UPDATE_SUCCESS_MESSAGE = 'Post mis à jour succes';
+const UKNOWN_FAILURE_MESSAGE = 'Erreur inconnue. Veuillez réessayer plus tard';
